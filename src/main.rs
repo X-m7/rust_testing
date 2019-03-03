7940e5d7b864dea74fe9502d884948c984d84dee
Prajna Sariputra <putr4.s@gmail.com>
2019-03-03 16:57:10 +1100

    Fixed warning in I/O part, extra formatting and comments
    Issue was that a Result was not handled, so use expect() for that

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ fn main() {
     println!("Hello, world!");
 
 	print!("Input: ");
-	io::stdout().flush();
-	let mut input = String::new();
-	io::stdin().read_line(&mut input).expect("Failed to read");
-	
+	io::stdout().flush()
+		.expect("Failed to flush"); //if there is a method that requires that a Result be handled expect() is one way to do so
+	let mut input = String::new(); //mut --> mutable
+	io::stdin().read_line(&mut input) //&input for immutable reference
+		.expect("Failed to read");
 	println!("Echoed Input: {}", input);
 }
