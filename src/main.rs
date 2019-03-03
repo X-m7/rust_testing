303230b42995c773b9962ee5fea4dd7f6c326242
Prajna Sariputra <putr4.s@gmail.com>
2019-03-03 16:40:20 +1100

    Basic I/O via console, warning present
    Need to figure out how to use result of flush()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::Write;
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+	print!("Input: ");
+	io::stdout().flush();
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).expect("Failed to read");
+	
+	println!("Echoed Input: {}", input);
 }
