d5bc680bc403ae80c3559f29b6974972c51f9d79
Prajna Sariputra <putr4.s@gmail.com>
2019-03-03 17:43:02 +1100

    Loop, multiple statements for match

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,24 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-	print!("Input: "); //need flush() afterwards so it is actually printed (in case of line buffering)
-	io::stdout().flush()
-		.expect("Failed to flush"); //if there is a method that requires that a Result be handled expect() is one way to do so
-	let mut input = String::new(); //mut --> mutable
-	io::stdin().read_line(&mut input) //&input for immutable reference
-		.expect("Failed to read");
-	let input: u32 = input.trim().parse() //u32 --> unsigned 32bit integer
-		.expect("Number expected");
-	let random_number = rand::thread_rng().gen_range(1,51); //crypto-grade apparently, range includes min and excludes max
-	println!("Random Number: {}", random_number);
-	match input.cmp(&random_number) {
-		Ordering::Less => println!("{} < {}", input, random_number),
-		Ordering::Greater => println!("{} > {}", input, random_number),
-		Ordering::Equal => println!("{} == {}", input, random_number),
+	loop { //infinite
+		print!("Input: "); //need flush() afterwards so it is actually printed (in case of line buffering)
+		io::stdout().flush()
+			.expect("Failed to flush"); //if there is a method that requires that a Result be handled expect() is one way to do so
+		let mut input = String::new(); //mut --> mutable
+		io::stdin().read_line(&mut input) //&input for immutable reference
+			.expect("Failed to read");
+		let input: u32 = input.trim().parse() //u32 --> unsigned 32bit integer
+			.expect("Number expected");
+		let random_number = rand::thread_rng().gen_range(1,51); //crypto-grade apparently, range includes min and excludes max
+		println!("Random Number: {}", random_number);
+		match input.cmp(&random_number) {
+			Ordering::Less => println!("{} < {}", input, random_number), //comma to end single statement
+			Ordering::Greater => println!("{} > {}", input, random_number),
+			Ordering::Equal => { //with braces for multiple
+				println!("{} == {}", input, random_number);
+				break;
+			}
+		}
 	}
 }
